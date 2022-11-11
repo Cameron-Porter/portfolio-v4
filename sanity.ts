0ed0daedcde013e94452c9f77b0dde5dce1bae5d
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-11-11 11:43:09 -0500

    fix: trying to resolve vercel build issue

--- sanity.ts
+++ sanity.ts
@@ -5,7 +5,6 @@ export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   useCdn: process.env.NODE_ENV === "production",
-  token: process.env.SANITY_API_TOKEN,
 };
 
 export const sanityClient = createClient(config);
