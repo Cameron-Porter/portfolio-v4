602007f794287cf5c10a97c7ffe95db4bb7efe06
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-11-14 14:29:35 -0500

    fix skill and skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { Skill } from "../typings";
+import { urlFor } from "../sanity";
 
 type Props = {
   directionLeft?: boolean;
+  skill: Skill;
 };
 
-export default function Skill({ directionLeft }: Props) {
+export default function Skill({ skill, directionLeft }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
       <motion.div
@@ -24,7 +27,7 @@ export default function Skill({ directionLeft }: Props) {
         }}
       >
         <Image
-          src="https://res.cloudinary.com/dz58encu1/image/upload/v1666809824/SealSpaceForce_etykly.png"
+          src={urlFor(skill.image).url()}
           alt=""
           height="150"
           width="150"
@@ -33,7 +36,9 @@ export default function Skill({ directionLeft }: Props) {
       </motion.div>
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">USSF</p>
+          <p className="text-3xl font-bold text-black opacity-100">
+            {skill.title}
+          </p>
         </div>
       </div>
     </div>
