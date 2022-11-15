59864333ca57270c215e4d6a1bc50eb8b2a075dc
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-11-15 08:40:11 -0500

    fix: footer display

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 export default function Footer({}: Props) {
   return (
-    <footer className="sticky bottom-10 w-full cursor-pointer">
+    <footer className="absolute bottom-10 w-full cursor-pointer">
       <div className="flex items-center justify-center">
         <Link href="#hero">
           <ChevronDoubleUpIcon className="h-10 w-10 rounded-full text-[#22f3ee]/70 hover:text-[#22f3ee] animate-pulse" />
